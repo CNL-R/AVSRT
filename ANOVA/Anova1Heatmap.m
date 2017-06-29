b610e2b5b4274e58cb8234a2dfd216df1f0c651d
ashoari <shoari@ece.rochester.edu>
2017-06-29 13:06:47 -0400

    ANOVA imshow presentation and clean up
    the heatmap structure changed useing imshow to enable xtick and ytick
    and also incorporate mapping requested by Kelly.

--- ANOVA/Anova1Heatmap.m
+++ ANOVA/Anova1Heatmap.m
@@ -1,13 +1,33 @@
-function [ Pmat,Sigmat, FiltSigmat ] = Anova1Heatmap(sampleperiod,adjsigframe,filtth,alphath,timecolumn,electrodecolumn,Title,XLabel,YLabel,expdata1 )
-%UNTITLED6 Summary of this function goes here
-%   Detailed explanation goes here
+% Developed by Arian Shoari. Copyright 2017 John Foxe's Lab, University of Rochester. 
+% Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License. You may obtain a copy of the License at
+% http://www.apache.org/licenses/LICENSE-2.0
+% Unless agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
+% https://www.apache.org/licenses/LICENSE-2.0
+
+function [ Pmat,Sigmat, FiltSigmat ] = Anova1Heatmap(sampleperiod,adjsigframe,filtth,alphath,timecolumn,electrodecolumn,Xtick,Xticklabel,Ytick,Yticklabel,Title,XLabel,YLabel,expdata1)
 % adjsigframe specify the number of adjacent significant violation time frame that requires to consider a violation in time a valid violation   
 % Pmat is the matrix of p values over time and electrode
 % Sigmat shows the significant of p values upon the alphath
 % FiltSigmat is the filtered Sigmat considering adjsigframe criteria in the time domain  
+% expdata1 is the input Matrix contains all the data
+% timecolumn,electrodecolumn are the columns of the matrix refering to the
+% time and electrode number of the samples
+% alphath is the significant level threshold for P values 
+% 
+% Xtick,Xticklabel,Ytick,Yticklabel are the tick points and labels to be
+% domonstared on the X, Y axis.
+% Title,XLabel,YLabel are the labels should be displayed on the heatmap
+% sampleperiod,adjsigframe,filtth are disabled in this version 
+% Original elecrode Mapping as follows
+% A1:A32  1:32
+% B1:B32  33:64
+% C1:C32  65:96
+% D1:D32  97:128
+% E1:E32  129:160
+% F1:F32  161:192
+% G1:G32  193:224
 
 tic;
-
 expsize=size(expdata1);
 tindexmax=expsize(1,timecolumn);
 electmax=expsize(1,electrodecolumn);
@@ -19,9 +39,6 @@ subjectcolumn=3;
 Pmat=zeros(tindexmax,electmax);
 Sigmat=zeros(tindexmax,electmax);
 FiltSigmat =zeros(tindexmax,electmax);
-
-
-
 
 for i=1:1:tindexmax,
     for j=1:electmax,
@@ -54,14 +71,61 @@ end
 % end
 
 illusmap=(Pmat.*Sigmat).';
-hmo=HeatMap(illusmap,'ColumnLabels',sampleperiod*[1:tindexmax],'Standardize',3,'Colormap',redbluecmap);
-addTitle(hmo, Title);
-addXLabel(hmo, XLabel);
-addYLabel(hmo, YLabel)
-% illusmap=(Pmat.*FiltSigmat).';
-% hmo=HeatMap(illusmap,'ColumnLabels',sampleperiod*[1:meassize(1,timecolumn)],'Standardize',3,'Colormap',redbluecmap);
-% addTitle(hmo, 'incongruent-congruent')
+% next line flip the columns to reflect the order requested i.e [D1:D32 C1:C32 E1:E32 G1:G32 B1:B32 F1:F32 A1:A32]
+OrderedIllusmap=[illusmap(97:128,:); illusmap(65:96,:); illusmap(129:160,:); illusmap(193:224,:); illusmap(33:64,:); illusmap(161:192,:); illusmap(1:32,:)];% because illusmap was transposed of Pmat
 
+%%
+
+hs=size(OrderedIllusmap);
+rows=hs(1,1);
+columns=hs(1,2);
+if  columns>rows,
+    scalec=floor(columns/rows);
+    scaler=1;
+else
+    scalec=1;
+    scaler=floor(rows/columns);
+end    
+newWidth = [1 scalec*rows];
+newLength= [1 scaler*columns];
+figure;
+
+if  columns>rows,
+    imshow(OrderedIllusmap,[] , 'YData',newWidth);
+else
+    imshow(OrderedIllusmap,[] , 'XData',newLength);
+end
+
+axis on;
+colormap(jet);
+colorbar;
+    
+ax = gca;
+ax.YTick=scalec*Ytick;
+ax.YTickLabel =Yticklabel;
+ax.XTick = scaler*Xtick;
+ax.XTickLabel = Xticklabel;
+
+ax.Title.String = Title;
+ax.XLabel.String =XLabel;
+ax.YLabel.String =YLabel;
+
+set(gca,'YDir','normal'); % flip bottom and top display on Y axis
+
+%% sample code that employs HeatMap function for drawing but can not colorbar or identify the ticks
+%  hmo=HeatMap(OrderedIllusmap,'ColumnLabels',sampleperiod*[1:tindexmax],'Standardize',3,'Colormap',jet); %  color map could be redbluecmap instead of jet
+%  addTitle(hmo, Title);
+%  addXLabel(hmo, XLabel);
+%  addYLabel(hmo, YLabel);
+% %%%
+
+%% sample code to resize the actual image! 
+%  stretchedImage =imresize(OrderedIllusmap,newWidth);
+%  imshow(stretchedImage , 'YData','');
+%  axis on;
+%  colormap(jet);
+%  colorbar
+% %%%
 %%
 toc
 end
--- ANOVA/Heatmap2.m
+++ ANOVA/Heatmap2.m
@@ -1,3 +1,9 @@
+% Developed by Arian Shoari. Copyright 2017 John Foxe's Lab, University of Rochester. 
+% Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License. You may obtain a copy of the License at
+% http://www.apache.org/licenses/LICENSE-2.0
+% Unless agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
+% https://www.apache.org/licenses/LICENSE-2.0
+
 sampleperiod=0.001; %
 adjsigframe=5; 
 filtth=0.5; %; real number between 0 and 1 
@@ -8,9 +14,15 @@ reducedresolution=10;
 
 contrastname={'switch-repeat','incongruent-congruent','respswitch-resprepeat'};
 
-meassize=size(ElecTimeCut);
+Ytick=[32 64 96 128 160 192 224];
+Yticklabel ={'D32','C32','E32','G32','B32','F32','A32'};
+XtickVec = [0 100 200 300  400 500 600 700 800 900 1000];
+Xticklabel = [0 100 200 300  400 500 600 700 800 900 1000];
+
 ElecTimeCut=incon_cong;
-[Pmat1,Sigmat1,FiltSigmat1] = Anova1Heatmap(sampleperiod,adjsigframe,filtth,alphath,timecolumn,electrodecolumn,'incongruent-congruent','time','electrode',ElecTimeCut );
+meassize=size(ElecTimeCut);
+Xtick=XtickVec;
+[Pmat1,Sigmat1,FiltSigmat1] = Anova1Heatmap(sampleperiod,adjsigframe,filtth,alphath,timecolumn,electrodecolumn,Xtick,Xticklabel,Ytick,Yticklabel,'incongruent-congruent','time','electrode',ElecTimeCut);
 
 
 timebinmax=floor(meassize(1,timecolumn)/reducedresolution);
@@ -26,14 +38,16 @@ for i=1:meassize(1,1),
         end  
     end
 end
-[Pmat2,Sigmat2,FiltSigmat2] = Anova1Heatmap(sampleperiod*reducedresolution,adjsigframe,filtth,alphath,timecolumn,electrodecolumn,'filtered incongruent-congruent','time','electrode',ElecTimeReduced );
+Xtick=floor((1/reducedresolution)*XtickVec);
+[Pmat2,Sigmat2,FiltSigmat2] = Anova1Heatmap(sampleperiod*reducedresolution,adjsigframe,filtth,alphath,timecolumn,electrodecolumn,Xtick,Xticklabel,Ytick,Yticklabel,'filtered incongruent-congruent','time (ms)','electrode',ElecTimeReduced );
 % addTitle(hmo, 'filtered incongruent-congruent')
 
 
 
 %%%%%%%%%%%%%%%%%%%%%%%%%
 ElecTimeCut=respsw_respre; 
-[Pmat3,Sigmat3,FiltSigmat3] = Anova1Heatmap(sampleperiod,adjsigframe,filtth,alphath,timecolumn,electrodecolumn,'respswitch-resprepeat','time','electrode',ElecTimeCut );
+Xtick=XtickVec;
+[Pmat3,Sigmat3,FiltSigmat3] = Anova1Heatmap(sampleperiod,adjsigframe,filtth,alphath,timecolumn,electrodecolumn,Xtick,Xticklabel,Ytick,Yticklabel,'respswitch-resprepeat','time','electrode',ElecTimeCut );
 
 ElecTimeReduced=zeros(meassize(1,1),meassize(1,2),meassize(1,3),timebinmax,meassize(1,5));
 for i=1:meassize(1,1),
@@ -47,11 +61,13 @@ for i=1:meassize(1,1),
         end  
     end
 end
-[Pmat4,Sigmat4,FiltSigmat4] = Anova1Heatmap(sampleperiod*reducedresolution,adjsigframe,filtth,alphath,timecolumn,electrodecolumn,'filtered respswitch-resprepeat','time','electrode',ElecTimeReduced );
+Xtick=floor((1/reducedresolution)*XtickVec);
+[Pmat4,Sigmat4,FiltSigmat4] = Anova1Heatmap(sampleperiod*reducedresolution,adjsigframe,filtth,alphath,timecolumn,electrodecolumn,Xtick,Xticklabel,Ytick,Yticklabel,'filtered respswitch-resprepeat','time','electrode',ElecTimeReduced );
 
 %%%%%%%%%%%%%%%%%%%%%%%%%
 ElecTimeCut=sw_rep; 
-[Pmat5,Sigmat5,FiltSigmat5] = Anova1Heatmap(sampleperiod,adjsigframe,filtth,alphath,timecolumn,electrodecolumn,'switch-repeat','time','electrode',ElecTimeCut );
+Xtick=XtickVec;
+[Pmat5,Sigmat5,FiltSigmat5] = Anova1Heatmap(sampleperiod,adjsigframe,filtth,alphath,timecolumn,electrodecolumn,Xtick,Xticklabel,Ytick,Yticklabel,'switch-repeat','time','electrode',ElecTimeCut );
 
 ElecTimeReduced=zeros(meassize(1,1),meassize(1,2),meassize(1,3),timebinmax,meassize(1,5));
 for i=1:meassize(1,1),
@@ -65,4 +81,5 @@ for i=1:meassize(1,1),
         end  
     end
 end
-[Pmat6,Sigmat6,FiltSigmat6] = Anova1Heatmap(sampleperiod*reducedresolution,adjsigframe,filtth,alphath,timecolumn,electrodecolumn,'filtered switch-repeat','time','electrode',ElecTimeReduced);
+Xtick=floor((1/reducedresolution)*XtickVec);
+[Pmat6,Sigmat6,FiltSigmat6] = Anova1Heatmap(sampleperiod*reducedresolution,adjsigframe,filtth,alphath,timecolumn,electrodecolumn,Xtick,Xticklabel,Ytick,Yticklabel,'filtered switch-repeat','time','electrode',ElecTimeReduced);
